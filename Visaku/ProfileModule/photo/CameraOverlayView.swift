accdf537245bd4d06636e0a1f7c4ac5d9952a3bc
Lonard Steven <86178738+lonard2@users.noreply.github.com>
2024-11-07 17:03:21 +0700

    fix: re-enable camera-related functions

--- Visaku/ProfileModule/photo/CameraOverlayView.swift
+++ Visaku/ProfileModule/photo/CameraOverlayView.swift
@@ -1,111 +1,111 @@
-////
-////  CameraOverlayView.swift
-////  CameraIntensityTest
-////
-////  Created by Lonard Steven on 05/11/24.
-////
 //
-//import SwiftUI
+//  CameraOverlayView.swift
+//  CameraIntensityTest
 //
-//struct CameraOverlayView: View {
-//    @Binding var goToCamera: Bool
-//    @Binding var shouldCaptureImage: Bool
-//    @ObservedObject var cameraState: CameraState
-//    
-//    var body: some View {
-//        VStack {
-//            HStack {
-//                Button(action: {
-//                    goToCamera.toggle()
-//                }) {
-//                    Circle()
-//                        .fill(.black.opacity(25.0))  .frame(width: 44, height: 44)
-//                        .overlay {
-//                            Image(systemName: "xmark")
-//                                .padding()
-//                        }
-//                }
-//                .frame(width: 80, height: 44)
-//                .foregroundStyle(.white)
-//                
-//                .padding(.leading, 4)
-//                Spacer()
-//            }
-//            
-//            .padding(.top, 16)
-//            Spacer()
-//            
-//            HStack {
-//                RoundedRectangle(cornerRadius: 16)
-//                    .fill(Color.white.opacity(0.7))
-//                    .frame(height: 50)
-//                    .overlay {
-//                        getMessageOverlay()
-//                    }
-//                    .padding(.horizontal, 16)
-//            }
-//            .padding(.bottom, 71)
+//  Created by Lonard Steven on 05/11/24.
 //
-//
-//            Button(action: {
-//                shouldCaptureImage = true
-//                
-//                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-//                    shouldCaptureImage = false
-//                }
-//            }) {
-//                
-//                
-//                Circle()
-//                    .fill(.white)
-//                    .frame(width: 66, height: 66)
-//                    .padding(.all, 5)
-//                    .overlay(
-//                        Circle()
-//                            .stroke(Color.white, lineWidth: 3)
-//                    )
-//                    .shadow(radius: 5)
-//            }
-//            .padding(.bottom, 35)
-//        }
-//    }
-//    
-//    private func getMessageOverlay() -> some View {
-//        Group {
-//            if !cameraState.isCameraFeedReady {
-//                HStack {
-//                    Text("Menunggu kamera diaktifkan... ")
-//                        .foregroundStyle(.black)
-//                        .font(.system(size: 18, weight: .medium))
-//                    
-//                    Image(systemName: "hourglass")
-//                        .foregroundStyle(.brown)
-//                }
-//            } else {
-//                if !cameraState.isFocused {
-//                    HStack {
-//                        Text("Kamera masih blur")
-//                            .foregroundStyle(.red)
-//                            .font(.system(size: 18, weight: .medium))
-//                    }
-//                } else if cameraState.offset < -0.25 {
-//                    HStack {
-//                        Text("Cahaya terlalu gelap")
-//                            .foregroundStyle(.red)
-//                            .font(.system(size: 18, weight: .medium))
-//                    }
-//                } else {
-//                        HStack {
-//                            Text("Foto sesuai")
-//                                .foregroundStyle(.black)
-//                                .font(.system(size: 18, weight: .medium))
-//                            
-//                            Image(systemName: "checkmark.circle.fill")
-//                                .foregroundStyle(.green)
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
+
+import SwiftUI
+
+struct CameraOverlayView: View {
+    @Binding var goToCamera: Bool
+    @Binding var shouldCaptureImage: Bool
+    @ObservedObject var cameraState: CameraState
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: {
+                    goToCamera.toggle()
+                }) {
+                    Circle()
+                        .fill(.black.opacity(25.0))  .frame(width: 44, height: 44)
+                        .overlay {
+                            Image(systemName: "xmark")
+                                .padding()
+                        }
+                }
+                .frame(width: 80, height: 44)
+                .foregroundStyle(.white)
+                
+                .padding(.leading, 4)
+                Spacer()
+            }
+            
+            .padding(.top, 16)
+            Spacer()
+            
+            HStack {
+                RoundedRectangle(cornerRadius: 16)
+                    .fill(Color.white.opacity(0.7))
+                    .frame(height: 50)
+                    .overlay {
+                        getMessageOverlay()
+                    }
+                    .padding(.horizontal, 16)
+            }
+            .padding(.bottom, 71)
+
+
+            Button(action: {
+                shouldCaptureImage = true
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    shouldCaptureImage = false
+                }
+            }) {
+                
+                
+                Circle()
+                    .fill(.white)
+                    .frame(width: 66, height: 66)
+                    .padding(.all, 5)
+                    .overlay(
+                        Circle()
+                            .stroke(Color.white, lineWidth: 3)
+                    )
+                    .shadow(radius: 5)
+            }
+            .padding(.bottom, 35)
+        }
+    }
+    
+    private func getMessageOverlay() -> some View {
+        Group {
+            if !cameraState.isCameraFeedReady {
+                HStack {
+                    Text("Menunggu kamera diaktifkan... ")
+                        .foregroundStyle(.black)
+                        .font(.system(size: 18, weight: .medium))
+                    
+                    Image(systemName: "hourglass")
+                        .foregroundStyle(.brown)
+                }
+            } else {
+                if !cameraState.isFocused {
+                    HStack {
+                        Text("Kamera masih blur")
+                            .foregroundStyle(.red)
+                            .font(.system(size: 18, weight: .medium))
+                    }
+                } else if cameraState.offset < -0.25 {
+                    HStack {
+                        Text("Cahaya terlalu gelap")
+                            .foregroundStyle(.red)
+                            .font(.system(size: 18, weight: .medium))
+                    }
+                } else {
+                        HStack {
+                            Text("Foto sesuai")
+                                .foregroundStyle(.black)
+                                .font(.system(size: 18, weight: .medium))
+                            
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundStyle(.green)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
