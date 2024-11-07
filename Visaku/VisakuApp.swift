70bbdc32edd41f70bedd5c1ec34a8ea4ccc39d35
Hendra Lijaya <hendralijaya@gmail.com>
2024-11-07 14:10:44 +0700

    Initial Commit

---
+++ Visaku/VisakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisakuApp.swift
+//  Visaku
+//
+//  Created by hendra on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VisakuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisakuTests/VisakuTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisakuTests.swift
+//  VisakuTests
+//
+//  Created by hendra on 07/11/24.
+//
+
+import Testing
+@testable import Visaku
+
+struct VisakuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
