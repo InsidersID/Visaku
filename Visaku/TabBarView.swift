bdd79d9325f98ddb4b1f1788dc0947aeb50670c6
Balya Elfata <balyaelfata@yahoo.com>
2024-11-12 22:54:04 +0700

    feat: added tagging for selected tabs

--- Visaku/TabBarView.swift
+++ Visaku/TabBarView.swift
@@ -2,18 +2,22 @@ import SwiftUI
 
 struct TabBarView: View {
     @Environment(\.colorScheme) var colorScheme
+    @State private var selectedTab = 0
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             VisaHistoryView()
                 .tabItem {
                     Label("Visaku", systemImage: "pencil.line")
                 }
-
+                .tag(0)
+                
             ProfileView()
                 .tabItem {
                     Label("Profil", systemImage: "person.crop.circle.fill")
                         .foregroundStyle(colorScheme == .dark ? Color.white : Color.black)
                 }
+                .tag(1)
         }
     }
 }
