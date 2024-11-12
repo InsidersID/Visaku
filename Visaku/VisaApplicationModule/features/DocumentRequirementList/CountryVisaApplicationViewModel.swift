f0297618a04b0c4119342b3cc7564c06bb715bff
nisrinous <nisrinous@gmail.com>
2024-11-13 01:30:47 +0700

    fix: add form application states to country visa application view model

--- Visaku/VisaApplicationModule/features/DocumentRequirementList/CountryVisaApplicationViewModel.swift
+++ Visaku/VisaApplicationModule/features/DocumentRequirementList/CountryVisaApplicationViewModel.swift
@@ -25,6 +25,46 @@ public class CountryVisaApplicationViewModel: ObservableObject {
         guard let requirements = trip?.visaRequirements else { return 0 }
         let markedCount = requirements.filter { $0.isMarked }.count
         return (Double(markedCount) / Double(requirements.count)) * 100
+    // Form data properties
+    @Published var hasOtherResidence: String? = ""
+    @Published var residenceType: String = ""
+    @Published var residenceNumber: String = ""
+    @Published var residenceExpiryDate: Date? = nil
+    
+    @Published var mainTravelPurpose: String? = ""
+    @Published var additionalTravelPurpose: String? = ""
+    
+    @Published var visaEntryCount: String? = ""
+    @Published var stayDuration: String = ""
+    
+    @Published var hasPreviousVisa: String? = ""
+    @Published var previousVisaDetails: String? = ""
+    @Published var previousVisaIssueDate: Date? = nil
+    @Published var previousVisaExpiryDate: Date? = nil
+    
+    @Published var providedFingerprint: String? = ""
+    @Published var fingerprintDate: Date? = nil
+    @Published var fingerprintVisaDetails: String = ""
+    @Published var lastSchengenEntryPlace: String = ""
+    
+    @Published var lastSchengenEntryCountry: String? = ""
+    @Published var lastSchengenEntryDate: Date? = nil
+    @Published var lastSchengenExitDate: Date? = nil
+    @Published var arrivalDate: Date? = nil
+    @Published var departureDate: Date? = nil
+    
+    @Published var travelPayer: String? = ""
+    @Published var livingCostForm: String? = ""
+    
+    @Published var euFamilyLastName: String = ""
+    @Published var euFamilyFirstName: String = ""
+    @Published var euFamilyBirthDate: Date? = nil
+    @Published var euFamilyNationality: String = ""
+    @Published var euFamilyDocumentNumber: String = ""
+    @Published var euFamilyRelation: String? = ""
+    
+    init(isMarkedStatus: [VisaGeneralTouristDocumentType: Bool]) {
+        self.isMarkedStatus = isMarkedStatus
     }
     
     public func saveTripData(visaType: String, countrySelected: String) {
