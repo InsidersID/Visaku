c192289b1fcd3d7d542e507d72543a1576e2fe55
Lonard Steven <86178738+lonard2@users.noreply.github.com>
2024-11-12 09:00:07 +0700

    feat: Make passport preview sheet as scrollable navigation stack (for toolbar)

--- Visaku/ProfileModule/passport/PassportPreviewSheet.swift
+++ Visaku/ProfileModule/passport/PassportPreviewSheet.swift
@@ -19,106 +19,107 @@ public struct PassportPreviewSheet: View {
     }
     
     public var body: some View {
-        VStack {
-            if let passportImage = passportPreviewViewModel.passportImage {
-                Image(uiImage: passportImage)
-                    .resizable()
-                    .frame(height: 200)
-                    .cornerRadius(24)
-                    .padding()
-            }
-            else {
-                Image(systemName: "dock.rectangle")
-                    .resizable()
-                    .frame(height: 200)
-                    .background(Color.red)
-                    .cornerRadius(24)
-                    .padding()
-            }
-            Divider()
-            HStack {
-                Text("Perhatikan hasil scan dokumen")
-                    .font(.headline)
-                Image(systemName: "chevron.right")
-                Spacer()
-            }
-            .padding(.horizontal)
-            
-            CardContainer(cornerRadius: 24) {
+        NavigationStack {
+            ScrollView {
                 VStack {
-                    Group {
-                        KeyValueDropdownRow<PassportType>(key: "Passport Type", selectedOption: $passportPreviewViewModel.passport.passportType)
-                        KeyValueDropdownRow<PassportIssueType>(key: "Passport Issue Type", selectedOption: $passportPreviewViewModel.passport.passportIssueType)
-                        KeyValueRow(key: "Tempat Penerbitan", value: $passportPreviewViewModel.passport.passportIssuePlace)
-                        KeyValueDateRow(key: "Tanggal Penerbitan", dateValue: $passportPreviewViewModel.passport.issueDate)
-                        KeyValueDateRow(key: "Tanggal Kadaluarsa", dateValue: $passportPreviewViewModel.passport.expirationDate)
-                        KeyValueRow(key: "Nomor Passport", value: $passportPreviewViewModel.passport.passportNo)
+                    if let passportImage = passportPreviewViewModel.passportImage {
+                        Image(uiImage: passportImage)
+                            .resizable()
+                            .frame(height: 200)
+                            .cornerRadius(24)
+                            .padding()
+                    }
+                    else {
+                        Image(systemName: "dock.rectangle")
+                            .resizable()
+                            .frame(height: 200)
+                            .background(Color.red)
+                            .cornerRadius(24)
+                            .padding()
+                    }
+                    Divider()
+                    HStack {
+                        Text("Perhatikan hasil scan dokumen")
+                            .font(.headline)
+                        Image(systemName: "chevron.right")
+                        Spacer()
                     }
                     .padding(.horizontal)
-                    .padding(.vertical, 4)
-                }
-            }
-            
-            VStack {
-                switch passportPreviewViewModel.savePassportState {
-                case .loading:
-                    ProgressView("Menyimpan...")
-                        .padding()
-                case .error:
-                    Text("Eror menyimpan paspormu").foregroundColor(.red)
-                case .success:
-                    Text("Paspormu sukses tersimpan").foregroundColor(.green)
-                case .idle:
-                    CustomButton(text: "Simpan", color: .blue, fontSize: 17, cornerRadius: 14, paddingHorizontal: 16, paddingVertical: 16) {
-                        Task {
-                            await passportPreviewViewModel.savePassport()
+                    
+                    CardContainer(cornerRadius: 24) {
+                        VStack {
+                            Group {
+                                KeyValueDropdownRow<PassportType>(key: "Passport Type", selectedOption: $passportPreviewViewModel.passport.passportType)
+                                KeyValueDropdownRow<PassportIssueType>(key: "Passport Issue Type", selectedOption: $passportPreviewViewModel.passport.passportIssueType)
+                                KeyValueRow(key: "Tempat Penerbitan", value: $passportPreviewViewModel.passport.passportIssuePlace)
+                                KeyValueDateRow(key: "Tanggal Penerbitan", dateValue: $passportPreviewViewModel.passport.issueDate)
+                                KeyValueDateRow(key: "Tanggal Kadaluarsa", dateValue: $passportPreviewViewModel.passport.expirationDate)
+                                KeyValueRow(key: "Nomor Passport", value: $passportPreviewViewModel.passport.passportNo)
+                            }
+                            .padding(.horizontal)
+                            .padding(.vertical, 4)
                         }
-                        dismiss()
                     }
-                }
-                
-                switch passportPreviewViewModel.deletePassportState {
-                case .loading:
-                    ProgressView("Menghapus...")
-                        .padding()
-                case .error:
-                    Text("Eror menghapus paspor").foregroundColor(.red)
-                case .success:
-                    Text("Paspormu sukses terhapus").foregroundColor(.green)
-                case .idle:
-                    CustomButton(text: "Hapus dokumen", textColor: .blue, color: .white, fontSize: 17, cornerRadius: 14, paddingHorizontal: 16, paddingVertical: 16) {
-                        Task {
-                            await passportPreviewViewModel.deletePassport()
+                    
+                    VStack {
+                        switch passportPreviewViewModel.savePassportState {
+                        case .loading:
+                            ProgressView("Menyimpan...")
+                                .padding()
+                        case .error:
+                            Text("Eror menyimpan paspormu").foregroundColor(.red)
+                        case .success:
+                            Text("Paspormu sukses tersimpan").foregroundColor(.green)
+                        case .idle:
+                            CustomButton(text: "Simpan", color: .blue, fontSize: 17, cornerRadius: 14, paddingHorizontal: 16, paddingVertical: 16) {
+                                Task {
+                                    await passportPreviewViewModel.savePassport()
+                                }
+                                dismiss()
+                            }
                         }
-                        dismiss()
+                        
+                        switch passportPreviewViewModel.deletePassportState {
+                        case .loading:
+                            ProgressView("Menghapus...")
+                                .padding()
+                        case .error:
+                            Text("Eror menghapus paspor").foregroundColor(.red)
+                        case .success:
+                            Text("Paspormu sukses terhapus").foregroundColor(.green)
+                        case .idle:
+                            CustomButton(text: "Hapus dokumen", textColor: .blue, color: .white, fontSize: 17, cornerRadius: 14, paddingHorizontal: 16, paddingVertical: 16) {
+                                Task {
+                                    await passportPreviewViewModel.deletePassport()
+                                }
+                                dismiss()
+                            }
+                        }
                     }
+                    
+                }
+                .frame(width: .infinity)
+            }
+            .fullScreenCover(isPresented: $passportPreviewViewModel.isCameraOpen, content: {
+                VNDocumentCameraViewControllerRepresentable(scanResult: $passportPreviewViewModel.passportImage)
+                    .ignoresSafeArea()
+            })
+            .onChange(of: passportPreviewViewModel.passportImage) { _, newValue in
+                if let unwrappedImage = newValue {
+                    passportPreviewViewModel.processCapturedImage(unwrappedImage)
+                } else {
+                    print("No image to process")
                 }
             }
-            
-        }
-        .frame(width: .infinity, height: .infinity)
-        .fullScreenCover(isPresented: $passportPreviewViewModel.isCameraOpen, content: {
-            VNDocumentCameraViewControllerRepresentable(scanResult: $passportPreviewViewModel.passportImage)
-                .ignoresSafeArea()
-        })
-        .onChange(of: passportPreviewViewModel.passportImage) { _, newValue in
-            if let unwrappedImage = newValue {
-                passportPreviewViewModel.processCapturedImage(unwrappedImage)
-            } else {
-                print("No image to process")
-            }
-        }
-        .navigationTitle("KTP")
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button(action: {
-                    dismiss()
-                }) {
-                    Image(systemName: "x.circle")
-                        .font(.title)
-                        .foregroundColor(.blue)
-                }
-            }
+            .navigationTitle("Paspor")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(trailing: Button(action: {
+                dismiss()
+            }) {
+                Image(systemName: "x.circle")
+                    .font(.title)
+                    .foregroundColor(.blue)
+            })
         }
     }
 }
