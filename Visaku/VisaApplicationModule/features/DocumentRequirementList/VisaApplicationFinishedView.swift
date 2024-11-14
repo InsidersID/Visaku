6335cdafd03e1b32b03dccf0ed3521084a1f7955
Balya Elfata <balyaelfata@yahoo.com>
2024-11-14 22:51:15 +0700

    chore: added VisaApplicationFinishedView

---
+++ Visaku/VisaApplicationModule/features/DocumentRequirementList/VisaApplicationFinishedView.swift
@@ -0,0 +1,58 @@
+import SwiftUI
+import UIComponentModule
+
+struct VisaApplicationFinishedView: View {
+    var body: some View {
+        ZStack {
+            Color(red: 0.8, green: 0.91, blue: 0.97).ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                ZStack {
+                    Gauge(value: 100, in: 0...100) {
+                        
+                    } currentValueLabel: {
+                        VStack {
+                            Text("\(Int(100))%")
+                            Text("Visa turis Italia")
+                                .foregroundStyle(.black)
+                                .font(
+                                    Font.custom("Inter", size: 28)
+                                        .weight(.semibold)
+                                )
+                        }
+                        .foregroundStyle(.blue)
+                        .padding(.bottom, 50)
+                    }
+                    .gaugeStyle(VisaApplicationProgressStyle(gaugeSize: 300))
+                }
+                
+                Text("Kamu sudah siap!")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .padding(.bottom)
+                
+                Text("Dengan itinerary, kedutaan jadi tahu kamu punya tujuan jelas dan nggak berencana tinggal lebih lama dari yang diizinkan.")
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                    .padding(.horizontal)
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                Divider()
+                
+                CustomButton(text: "Selesai", textColor: .blue, color: .white) {
+                    
+                }
+                .padding(.horizontal)
+                .padding(.bottom)
+            }
+        }
+    }
+}
+
+#Preview {
+    VisaApplicationFinishedView()
+}
