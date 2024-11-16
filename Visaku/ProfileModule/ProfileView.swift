f5ccb0f6131fb89cb95c086bb7352af72e901754
Balya Elfata <balyaelfata@yahoo.com>
2024-11-16 15:20:02 +0700

    fix: fixed add profile modal logic and visual

--- Visaku/ProfileModule/ProfileView.swift
+++ Visaku/ProfileModule/ProfileView.swift
@@ -14,54 +14,61 @@ public struct ProfileView: View {
     
     public var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.flexible(), spacing: 20), GridItem(.flexible())], spacing: 20){
-                    if let accounts = profileViewModel.accounts {
-                        ForEach(accounts, id: \.id) { account in
-                            Button {
-                                if isSelectProfile {
-                                    visaViewModel.selectedIdentity = account
-                                    print(visaViewModel.selectedIdentity?.username ?? "error selecting identity")
-                                    visaViewModel.isIdentity.toggle()
-                                } else {
-                                    profileViewModel.selectedAccount = account
-                                    profileViewModel.navigateToMainDocuments = true
+            ZStack {
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(.flexible(), spacing: 20), GridItem(.flexible())], spacing: 20){
+                        if let accounts = profileViewModel.accounts {
+                            ForEach(accounts, id: \.id) { account in
+                                Button {
+                                    if isSelectProfile {
+                                        visaViewModel.selectedIdentity = account
+                                        print(visaViewModel.selectedIdentity?.username ?? "error selecting identity")
+                                        visaViewModel.isIdentity.toggle()
+                                    } else {
+                                        profileViewModel.selectedAccount = account
+                                        profileViewModel.navigateToMainDocuments = true
+                                    }
+                                } label: {
+                                    ProfileCard(name: account.username, isAddProfile: false, imageData: account.image)
                                 }
+                            }
+                        }
+                        if !isSelectProfile {
+                            Button {
+                                profileViewModel.isAddingProfile = true
                             } label: {
-                                ProfileCard(name: account.username, isAddProfile: false, imageData: account.image)
+                                ProfileCard(name: "Profil Baru", isAddProfile: true)
                             }
                         }
                     }
-                    if !isSelectProfile {
-                        Button {
-                            profileViewModel.isAddingProfile = true
-                        } label: {
-                            ProfileCard(name: "Profil Baru", isAddProfile: true)
-                        }
+                    .padding(.horizontal)
+                    .padding(.top)
+                }
+                .onAppear {
+                    Task {
+                        await profileViewModel.fetchAccount()
                     }
                 }
-                .padding(.horizontal)
-                .padding(.top)
-            }
-            .onAppear {
-                Task {
-                    await profileViewModel.fetchAccount()
-                }
-            }
-            .navigationTitle("Profil")
-            .alert(isPresented: $profileViewModel.isError, error: profileViewModel.error, actions: {
-                
-            })
-            .fullScreenCover(isPresented: $profileViewModel.isAddingProfile, content: {
-                AddProfileView(isEditing: false)
-                    .environment(profileViewModel)
-                    .clearModalBackground()
-            })
-            .navigationDestination(isPresented: $profileViewModel.navigateToMainDocuments) {
-                if let account = profileViewModel.selectedAccount {
-                    MainDocumentView(name: account.username, accountId: account.id)
-                        .navigationBarBackButtonHidden()
+                .navigationTitle("Profil")
+                .alert(isPresented: $profileViewModel.isError, error: profileViewModel.error, actions: {
+                    
+                })
+                .fullScreenCover(isPresented: $profileViewModel.isAddingProfile, content: {
+                    AddProfileView(isEditing: false)
                         .environment(profileViewModel)
+                        .clearModalBackground()
+                })
+                .navigationDestination(isPresented: $profileViewModel.navigateToMainDocuments) {
+                    if let account = profileViewModel.selectedAccount {
+                        MainDocumentView(name: account.username, accountId: account.id)
+                            .navigationBarBackButtonHidden()
+                            .environment(profileViewModel)
+                    }
+                }
+                
+                if profileViewModel.isAddingProfile {
+                    Color.black.opacity(0.75)
+                        .ignoresSafeArea()
                 }
             }
         }
