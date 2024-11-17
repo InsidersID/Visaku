bed507ae39d56d06ce854b83006b74ab24c39fcd
Balya Elfata <balyaelfata@yahoo.com>
2024-11-17 18:34:05 +0700

    feat: added color sets to Color extension

--- Visaku/Extension/Color.swift
+++ Visaku/Extension/Color.swift
@@ -9,28 +9,58 @@ import Foundation
 import SwiftUICore
 
 extension Color {
-    static var info1: Color = Color("info").opacity(0.1)
-    static var info2: Color = Color("info").opacity(0.25)
-    static var info3: Color = Color("info").opacity(0.5)
-    static var info4: Color = Color("info").opacity(0.75)
-    static var info5: Color = Color("info").opacity(1)
+    static var blackOpacity1: Color = Color("blackOpacity1")
+    static var blackOpacity2: Color = Color("blackOpacity2")
+    static var blackOpacity3: Color = Color("blackOpacity3")
+    static var blackOpacity4: Color = Color("blackOpacity4")
+    static var blackOpacity5: Color = Color("blackOpacity5")
     
-    static var danger1: Color = Color("danger").opacity(0.1)
-    static var danger2: Color = Color("danger").opacity(0.25)
-    static var danger3: Color = Color("danger").opacity(0.5)
-    static var danger4: Color = Color("danger").opacity(0.75)
-    static var danger5: Color = Color("danger").opacity(1)
+    static var border: Color = Color("border")
+    
+    static var danger1: Color = Color("danger1")
+    static var danger2: Color = Color("danger2")
+    static var danger3: Color = Color("danger3")
+    static var danger4: Color = Color("danger4")
+    static var danger5: Color = Color("danger5")
+    
+    static var info1: Color = Color("info")
+    static var info2: Color = Color("info")
+    static var info3: Color = Color("info")
+    static var info4: Color = Color("info")
+    static var info5: Color = Color("info")
     
     static var primary1: Color = Color("primary1")
     static var primary2: Color = Color("primary2")
     static var primary3: Color = Color("primary3")
     static var primary4: Color = Color("primary4")
     static var primary5: Color = Color("primary5")
+    static var primary5Opacity4: Color = Color("primary5Opacity4")
+    static var primary6: Color = Color("primary6")
+    static var primaryTint6: Color = Color("primaryTint6")
     
-    static var success1: Color = Color("success").opacity(0.1)
-    static var success2: Color = Color("success").opacity(0.25)
-    static var success3: Color = Color("success").opacity(0.5)
-    static var success4: Color = Color("success").opacity(0.75)
-    static var success5: Color = Color("success").opacity(1)
+    static var secondary4: Color = Color("secondary4")
+    static var secondary6: Color = Color("secondary6")
     
+    static var stayBlack: Color = Color("stayBlack")
+    static var stayWhite: Color = Color("stayWhite")
+
+    static var success1: Color = Color("success1")
+    static var success2: Color = Color("success2")
+    static var success3: Color = Color("success3")
+    static var success4: Color = Color("success4")
+    static var success5: Color = Color("success5")
+    static var success6: Color = Color("success6")
+    
+    static var successOpacity1: Color = Color("successOpacity1")
+    static var successOpacity2: Color = Color("successOpacity2")
+    static var successOpacity3: Color = Color("successOpacity3")
+    static var successOpacity4: Color = Color("successOpacity4")
+    
+    static var tertiary1: Color = Color("tertiary1")
+    static var tertiary3: Color = Color("tertiary3")
+    static var tertiary7: Color = Color("tertiary7")
+    
+    static var warning: Color = Color("warning")
+    
+    static var white: Color = Color("white")
 }
