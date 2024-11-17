35b93b5ce06f396135591827078daaddda29e461
Balya Elfata <balyaelfata@yahoo.com>
2024-11-17 23:52:37 +0700

    chore: moved document actions and document details to it's own struct for better debugging

--- Visaku/ProfileModule/DocumentDetailsView.swift
+++ Visaku/ProfileModule/DocumentDetailsView.swift
@@ -6,10 +6,8 @@ public struct DocumentDetailsView: View {
     @Environment(\.dismiss) var dismiss
     @Environment(ProfileViewModel.self) var profileViewModel
     public var document: String
-    @State private var isSeeDetails: Bool = false
     @StateObject public var account: AccountEntity
     @State public var status: DocumentStatus
-    @State private var passportDetailsIndex = 1
     
     public init (document: String, account: AccountEntity? = nil, status: DocumentStatus = .undone) {
         self.document = document
@@ -26,131 +24,153 @@ public struct DocumentDetailsView: View {
                         .padding(.top)
                         .padding(.vertical)
                     
-                    if !isSeeDetails {
-                        VStack {
-                            HStack {
-                                Button {
-                                    profileViewModel.uploadDocument = .init(name: document)
-                                    dismiss()
-                                } label: {
-                                    ZStack {
-                                        Circle()
-                                            .frame(width: 22)
-                                            .foregroundStyle(Color.primary5)
-                                        
-                                        Image(systemName: "square.and.arrow.up")
-                                            .resizable()
-                                            .frame(width: 11, height: 13)
-                                            .foregroundStyle(.white)
-                                            .fontWeight(.bold)
-                                            .offset(x: 0, y: -1)
-                                    }
-                                    
-                                    Text("Upload dari device")
-                                        .font(Font.custom("Inter-Regular", size: 16))
-                                        .foregroundStyle(.black)
-                                }
-                                
-                                
-                                Spacer()
-                            }
-                            .padding(.horizontal)
-                            .padding(.bottom)
-                            
-                            HStack {
-                                Button {
-                                    dismiss()
-                                    switch document {
-                                    case "KTP":
-                                        profileViewModel.isScanKTP = true
-                                        
-                                    case "Paspor":
-                                        profileViewModel.isScanPaspor = true
-                                        
-                                    case "Foto":
-                                        profileViewModel.isScanFoto = true
-                                    default:
-                                        dismiss()
-                                    }
-                                } label: {
-                                    ZStack {
-                                        Circle()
-                                            .frame(width: 22)
-                                            .foregroundStyle(Color.primary5)
-                                        
-                                        Image(systemName: "camera")
-                                            .resizable()
-                                            .frame(width: 14, height: 12)
-                                            .foregroundStyle(.white)
-                                            .fontWeight(.bold)
-                                            .offset(x: 0, y: -1)
-                                    }
-                                    
-                                    Text(document == "Foto" ? "Ambil foto" : "Scan dokumen")
-                                        .font(Font.custom("Inter-Regular", size: 16))
-                                        .foregroundStyle(.black)
-                                }
-                                
-                                Spacer()
-                            }
-                            .padding(.horizontal)
-                            .padding(.bottom)
-                            
-                            HStack {
-                                Button {
-                                    isSeeDetails = true
-                                } label: {
-                                    ZStack {
-                                        Circle()
-                                            .frame(width: 22)
-                                            .foregroundStyle(Color.primary5)
-                                        
-                                        Image(systemName: "eye")
-                                            .resizable()
-                                            .frame(width: 14, height: 11)
-                                            .foregroundStyle(.white)
-                                            .fontWeight(.bold)
-                                    }
-                                    
-                                    Text("Lihat ketentuan")
-                                        .font(Font.custom("Inter-Regular", size: 16))
-                                        .foregroundStyle(.black)
-                                }
-                                
-                                Spacer()
-                            }
-                            .padding(.horizontal)
-                        }
+                    if !profileViewModel.isSeeDetails {
+                        DocumentActions(document: document)
                         .padding()
                         
                     } else {
-                        VStack {
-                            Image(document == "Foto" ? "photo_checker" : "documents_folder")
-                                .resizable()
-                                .scaledToFit()
-                                .padding()
-                            
-                            Text(document != "Paspor" ? "Yuk, ambil foto terbaikmu untuk pengajuan visa. Tenang, ada guide untuk pengambilan fotonya." : passportDetailsIndex == 1 ? "Jangan lupa bawa 1 paspor dan fotokopi yang berlaku minimal 3 bulan setelah tinggal di Schengen (6 bulan untuk Spanyol)" : "Selain itu, paspor harus memiliki 2 halaman kosong, diterbitkan dalam 10 tahun terakhir, dan ditandatangani.")
-                                .foregroundStyle(Color("blackOpacity3"))
-                                .multilineTextAlignment(.center)
-                                .padding()
-                            
-                            CustomButton(text: document == "Paspor" && passportDetailsIndex == 1 ? "Selanjutnya" : "OK", color: document == "Paspor" && passportDetailsIndex == 1 ? Color("primary5Opacity4") : .primary5) {
-                                if document == "Paspor" && passportDetailsIndex == 1 {
-                                    passportDetailsIndex += 1
-                                } else {
-                                    passportDetailsIndex = 1
-                                    isSeeDetails = false
-                                }
-                            }
-                            .padding()
-                        }
+                        SeeDocumentDetails(document: document)
                     }
                 }
                 .frame(width: proxy.size.width, alignment: .center)
             }
         }
-        .presentationDetents([.fraction(isSeeDetails ? 0.7 : 0.3)])
+        .presentationDetents([.fraction(profileViewModel.isSeeDetails ? 0.7 : 0.3)])
+    }
+}
+
+struct DocumentActions: View {
+    @Environment(\.dismiss) var dismiss
+    @Environment(ProfileViewModel.self) var profileViewModel
+    public var document: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    profileViewModel.uploadDocument = .init(name: document)
+                    dismiss()
+                } label: {
+                    ZStack {
+                        Circle()
+                            .frame(width: 22)
+                            .foregroundStyle(Color.primary5)
+                        
+                        Image(systemName: "square.and.arrow.up")
+                            .resizable()
+                            .frame(width: 11, height: 13)
+                            .foregroundStyle(.white)
+                            .fontWeight(.bold)
+                            .offset(x: 0, y: -1)
+                    }
+                    
+                    Text("Upload dari device")
+                        .font(Font.custom("Inter-Regular", size: 16))
+                        .foregroundStyle(.black)
+                }
+                
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+            
+            HStack {
+                Button {
+                    dismiss()
+                    switch document {
+                    case "KTP":
+                        profileViewModel.isScanKTP = true
+                        
+                    case "Paspor":
+                        profileViewModel.isScanPaspor = true
+                        
+                    case "Foto":
+                        profileViewModel.isScanFoto = true
+                    default:
+                        dismiss()
+                    }
+                } label: {
+                    ZStack {
+                        Circle()
+                            .frame(width: 22)
+                            .foregroundStyle(Color.primary5)
+                        
+                        Image(systemName: "camera")
+                            .resizable()
+                            .frame(width: 14, height: 12)
+                            .foregroundStyle(.white)
+                            .fontWeight(.bold)
+                            .offset(x: 0, y: -1)
+                    }
+                    
+                    Text(document == "Foto" ? "Ambil foto" : "Scan dokumen")
+                        .font(Font.custom("Inter-Regular", size: 16))
+                        .foregroundStyle(.black)
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+            
+            HStack {
+                Button {
+                    profileViewModel.isSeeDetails = true
+                } label: {
+                    ZStack {
+                        Circle()
+                            .frame(width: 22)
+                            .foregroundStyle(Color.primary5)
+                        
+                        Image(systemName: "eye")
+                            .resizable()
+                            .frame(width: 14, height: 11)
+                            .foregroundStyle(.white)
+                            .fontWeight(.bold)
+                    }
+                    
+                    Text("Lihat ketentuan")
+                        .font(Font.custom("Inter-Regular", size: 16))
+                        .foregroundStyle(.black)
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct SeeDocumentDetails: View {
+    @Environment(\.dismiss) var dismiss
+    @Environment(ProfileViewModel.self) var profileViewModel
+    public var document: String
+    
+    var body: some View {
+        VStack {
+            Image(document == "Foto" ? "photo_checker" : "documents_folder")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            Text(document != "Paspor" ? "Yuk, ambil foto terbaikmu untuk pengajuan visa. Tenang, ada guide untuk pengambilan fotonya." : profileViewModel.passportDetailsIndex == 1 ? "Jangan lupa bawa 1 paspor dan fotokopi yang berlaku minimal 3 bulan setelah tinggal di Schengen (6 bulan untuk Spanyol)" : "Selain itu, paspor harus memiliki 2 halaman kosong, diterbitkan dalam 10 tahun terakhir, dan ditandatangani.")
+                .foregroundStyle(Color("blackOpacity3"))
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            CustomButton(
+                text: document == "Paspor" && profileViewModel.passportDetailsIndex == 1 ? "Selanjutnya" : "OK",
+                color: (document == "Paspor" && profileViewModel.passportDetailsIndex == 1) ? Color("primary5Opacity4") : .primary5) {
+                if document == "Paspor" && profileViewModel.passportDetailsIndex == 1 {
+                    profileViewModel.passportDetailsIndex += 1
+                } else {
+                    profileViewModel.passportDetailsIndex = 1
+                    profileViewModel.isSeeDetails = false
+                }
+            }
+            .padding()
+        }
     }
 }
 
