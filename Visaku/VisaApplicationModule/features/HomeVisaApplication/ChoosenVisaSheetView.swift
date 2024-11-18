790476bf9cb2648502159d3fad12cac78b32b0da
Balya Elfata <balyaelfata@yahoo.com>
2024-11-18 11:20:00 +0700

    style: choosen visa sheet color fix

--- Visaku/VisaApplicationModule/features/HomeVisaApplication/ChoosenVisaSheetView.swift
+++ Visaku/VisaApplicationModule/features/HomeVisaApplication/ChoosenVisaSheetView.swift
@@ -16,11 +16,11 @@ struct ChoosenVisaSheetView: View {
             VStack(spacing: 8) {
                 Text("Visa yang kamu akan ajukan adalah")
                     .font(.custom("Inter-Regular", size: 14))
-                    .foregroundStyle(.secondary)
+                    .foregroundStyle(Color.blackOpacity3)
                     .frame(maxWidth: .infinity, alignment: .leading)
                 Text("Visa \(choosenVisaType) \(choosenCountry)")
                     .font(.custom("Inter-SemiBold", size: 20))
-                    .bold()
+                    .foregroundStyle(Color.blackOpacity5)
                     .frame(maxWidth: .infinity, alignment: .leading)
             }
             .padding()
