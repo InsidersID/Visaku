7718bb78be83d5d7f695ef1ae25cb925cbf21aca
Balya Elfata <balyaelfata@yahoo.com>
2024-11-19 21:12:53 +0700

    merge: implement face id on tab bar view (#54)
    * feat: request face id access
    
    * feat: implement face id on tab bar view

--- Visaku/TabBarViewModel.swift
+++ Visaku/TabBarViewModel.swift
@@ -1,4 +1,35 @@
 import Foundation
+import LocalAuthentication
+
+@MainActor
+@Observable
+class TabBarViewModel{
+    var isUnlocked: Bool = false
+    var navigateToContentUnavailableView: Bool = false
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+            let reason = "Harap buka kunci perangkat untuk mengakses aplikasi"
+            
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        self.navigateToContentUnavailableView = true
+                        print("Authentication failed: \(authenticationError?.localizedDescription ?? "Unknown error")")
+                    }
+                }
+            }
+        } else {
+            // Handle the case where biometrics are not available
+            print("Biometrics are not available: \(error?.localizedDescription ?? "Unknown error")")
+        }
+    }
+}
 
 class AgreementManager {
     private let agreementKey = "userHasAgreed"
